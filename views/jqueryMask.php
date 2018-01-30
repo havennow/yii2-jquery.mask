cddbf2a133daaa2e1989196d1f7cd2e7a82a7156
Guilherme <havennow@gmail.com>
2018-01-30 18:53:47 -0200

    Fix options for IE

--- views/jqueryMask.php
+++ views/jqueryMask.php
@@ -9,13 +9,23 @@ echo Html::activeInput($type, $model, $attribute, $htmlOptions);
 
 $_id = Html::getInputId($model, $attribute);
 
+$JS_OP = <<< JS
+
+$('#{$_id}').mask('{$mask}', {$options});
+
+JS;
+
 $JS = <<< JS
 
 $('#{$_id}').mask('{$mask}', {$options});
 
 JS;
 
-$this->registerJs($JS);
+if (empty($options)) {
+    $this->registerJs($JS);
+}else{
+    $this->registerJs($JS_OP);
+}
 
 
 
